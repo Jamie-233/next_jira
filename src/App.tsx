e7f8c96bf3d74dd1118b542b914e38fa2aae97c1
Jenkin <zhaojiancun@yunquna.com>
2021-05-20 17:20:50 +0800

    chore: added jsonserver middleware file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import ProjectList from "pages/project-list";
+import Login from "pages/login";
+
 import "App.css";
 
 function App() {
   return (
     <div className="App">
-      <ProjectList />
+      <Login />
+      {/* <ProjectList /> */}
     </div>
   );
 }
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,45 @@
+import { FormEvent } from "react";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+const Login = () => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("event", event);
+    const username = (event.currentTarget.elements[0] as HTMLInputElement)
+      .value;
+    const password = (event.currentTarget.elements[1] as HTMLInputElement)
+      .value;
+    login({ username, password });
+  };
+
+  const login = (param: { username: string; password: string }) => {
+    fetch(`${apiUrl}/login`, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (response) => {
+      if (response.ok) {
+        console.log("oks");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">UserName</label>
+        <input type="text" id={"username"} />
+      </div>
+      <div>
+        <label htmlFor="password">PassWord</label>
+        <input type="password" id={"password"} />
+      </div>
+      <button type={"submit"}>Login</button>
+    </form>
+  );
+};
+
+export default Login;
