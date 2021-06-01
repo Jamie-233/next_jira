e40db1580a93b92fc102a96f8560c10b683b210f
Jenkin <jiancunge.com@gmail.com>
2021-06-01 12:57:06 +0800

    feat: added id-select component

---
+++ src/components/id-select.tsx
@@ -0,0 +1,41 @@
+import { Select } from "antd";
+import { Row } from "types";
+
+type SelectProps = React.ComponentProps<typeof Select>;
+
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "options" | "onChange"> {
+  value: Row | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+/**
+ * value accept multiple types
+ * onChange only callback number or string type
+ * is isNan(Number(value)) equal true means select default value
+ * when the select default value onChange return undefined
+ */
+
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, options, defaultOptionName, ...restProps } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option key={option.id} value={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
